9d8326ea99d24d9fd38a972bde2bec95ab9e1b0c
Ronan <2849131603@qq.com>
2026-07-13 21:21:13 +0800

    Fix account actions and VIP profile parsing

--- app/src/test/java/com/ronan/qmusicwatch/UserProfileParserTest.kt
+++ app/src/test/java/com/ronan/qmusicwatch/UserProfileParserTest.kt
@@ -7,11 +7,23 @@ import org.junit.Test
 
 class UserProfileParserTest {
     @Test fun parsesNicknameAndProtocolRelativeAvatar() {
-        val profile = parseUserProfile(Json.parseToJsonElement("""{"creator":{"nick":"Ronan","headpic":"//img.qq.com/a.png"},"background":{"picurl":"https://img.qq.com/wallpaper.jpg"},"greenVip":{"isVip":0},"superVip":{"isSVip":1,"vipEndTime":1893456000}}"""))!!
+        val profile = parseUserProfile(Json.parseToJsonElement("""{"creator":{"nick":"Ronan","logo":"//img.qq.com/a.png"},"background":{"picurl":"https://img.qq.com/wallpaper.jpg"},"greenVip":{"isVip":0},"superVip":{"isSVip":1,"vipEndTime":1893456000}}"""))!!
         assertEquals("Ronan", profile.displayName)
         assertEquals("https://img.qq.com/a.png", profile.avatarUrl)
         assertEquals(true, profile.isVip)
         assertEquals(1893456000L, profile.vipExpireAt)
-        assertEquals("超级会员", profile.vipName)
+        assertEquals("超级会员（SVIP）", profile.vipName)
+    }
+
+    @Test fun ignoresSongVipFlagsWhenParsingAccount() {
+        val profile = parseUserProfile(Json.parseToJsonElement("""{"info":{"nick":"Ronan","logo":"https://img.qq.com/a.png"},"song":{"title":"测试歌曲","isVip":0}}"""))!!
+        assertEquals(null, profile.isVip)
+    }
+
+    @Test fun parsesDedicatedVipResponse() {
+        val profile = parseUserProfile(Json.parseToJsonElement("""{"vip_info":{"vip_type":11,"vip_name":"超级会员","expire_date":"20260722"}}"""))!!
+        assertEquals(true, profile.isVip)
+        assertEquals("超级会员（SVIP）", profile.vipName)
+        assertEquals(1784649600L, profile.vipExpireAt)
     }
 }
